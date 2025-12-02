3d9ff2fb73fc356302e2c62fb2ce162d6820ce59
33dataless <roman_vrazov@outlook.com>
2025-12-02 21:03:41 +0900

    first version dota parser

--- main_parser/mainparser.py
+++ main_parser/mainparser.py
@@ -1,32 +1,57 @@
 import requests as rq
+from requests import Response
 import json
 
 class DotaParser:
     def __init__(self):
         self.__link_all_position__ = "https://dota2protracker.com/api/heroes/stats?mmr=7000&position=all&order_by=matches&min_matches=20&period=8&legacy=false"
+        self.__link_1pos_position__ = "https://dota2protracker.com/api/heroes/stats?mmr=7000&position=pos%201&order_by=matches&min_matches=20&period=8&legacy=false"
+        self.__link_2pos_position__ = "https://dota2protracker.com/api/heroes/stats?mmr=7000&position=pos%202&order_by=matches&min_matches=20&period=8&legacy=false"
+        self.__link_3pos_position__ = "https://dota2protracker.com/api/heroes/stats?mmr=7000&position=pos%203&order_by=matches&min_matches=20&period=8&legacy=false"
+        self.__link_4pos_position__ = "https://dota2protracker.com/api/heroes/stats?mmr=7000&position=pos%204&order_by=matches&min_matches=20&period=8&legacy=false"
+        self.__link_5pos_position__ = "https://dota2protracker.com/api/heroes/stats?mmr=7000&position=pos%205&order_by=matches&min_matches=20&period=8&legacy=false"
 
+
+    def get_response(self, link: str) -> Response:
+        return rq.get(link)
     
-    def dump_response(self, time: str):
-        with open(f"D:\\pyproject\\DotaParser\\main_parser\\response\\all_position_{time}.json", "w") as file:
-            json.dump(rq.get(self.__link_all_position__).text, file, ensure_ascii=False, indent=4)
-
+    """for debug"""
     @staticmethod
-    def log(jdata, id):
-        print(f"""id героя - {jdata[id]["hero_id"]} | name - {jdata[id]["hero_name"]} | аспект - {jdata[id]["hero_variant"]} | {jdata[id]["facet_name"]} 
-            mathes - {jdata[id]["matches"]} | wins - {jdata[id]["wins"]}, | winrate - {jdata[id]["contest_rate"]}""")
-        
-    
+    def print_vector(vector: list):
+        for elem in vector:
+            print(elem)
 
-    def get_winrate_all_position(self):
-        data = rq.get(self.__link_all_position__)
-        if data.status_code == 200:
-            jdata = json.loads(data.text)
-            id = -1
-            while True:
+    def get_winrate(self, wins, matches):
+        return (wins/matches)*100
+
+    def create_vector(self, response: Response):
+        status_code = response.status_code
+        if status_code == 200:
+            data = json.loads(response.text)
+            id = 0
+            vector = []
+            for data in data:
+                vector.append([id, data["hero_id"], data["hero_name"], data["d2pt_rating"], data["facet_name"], data["matches"], data["wins"], data["position"], data["updated_at"]])
                 id += 1
-                if jdata[id]["facet_name"] == 'Shadowhawk':
-                    self.log(jdata, id)
-                    break
+            else:
+                vector = sorted(vector, key=lambda row: row[3], reverse=True)[0:10]        
+                answer = []
+                for elem in vector:
+                    winrate = self.get_winrate(elem[6] ,elem[5])
+                    answer.append([elem[2], elem[3], elem[4], elem[5], elem[6], int(winrate)])
                 else:
-                    self.log(jdata, id)
+                    return answer      
+        else:  
+            print(f"error | status code > {status_code}")
+            return None
+    
+    """for debug"""
+    @staticmethod
+    def dump_response(time: str, position: str, response: Response):
+        with open(f"D:\\pyproject\\DotaParser\\main_parser\\response\\{position}_{time}.json", "w") as file:
+            json.dump(response.text, file, ensure_ascii=False, indent=2)
+
+        
+
+
                    
--- test_parser.py
+++ test_parser.py
@@ -2,4 +2,9 @@ from main_parser.mainparser import DotaParser
 from util.get_time_now import get_time_now
 
 dotaparser = DotaParser()
-dotaparser.dump_response(get_time_now(format_file=True))
+response = dotaparser.get_response(link=dotaparser.__link_all_position__)
+vector = dotaparser.create_vector(response=response)
+dotaparser.print_vector(vector=vector)
+
+# dotaparser.dump_response(response=response, position="5pos", time=get_time_now(format_file=True))
+
