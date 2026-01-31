054bf94dbbc26906c6ad1783a3505e2328fb09f0
33dataless <roman_vrazov@outlook.com>
2026-01-31 18:07:00 +0900

    r

--- handlers/registr.py
+++ handlers/registr.py
@@ -2,11 +2,10 @@ from aiogram import Dispatcher
 
 from middlewares.middleware import DataBaseMiddleWare
 
-from .start import router as cmd_start_router
 from .callback import router as callback_router
-
+from .menu import router as menu_router
 
 def register_handlers(dp: Dispatcher):
     dp.update.outer_middleware(DataBaseMiddleWare())
-    dp.include_router(cmd_start_router)
     dp.include_router(callback_router)
+    dp.include_router(menu_router)
